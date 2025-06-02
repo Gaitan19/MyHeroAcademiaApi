01b0826835c74e0a475d30f047e2c0e6fc8c41c7
kenley <gaitan19>
2025-06-02 14:56:25 -0600

    Configure CORS policy to allow all origins, headers, and methods

--- Program.cs
+++ Program.cs
@@ -32,6 +32,16 @@ builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); // <- Esto es fundamental
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -40,9 +50,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(); // <- Esto genera la UI en /swagger/index.html
 }
 
+app.UseSwagger(); // <- Esto genera /swagger/v1/swagger.json
+app.UseSwaggerUI(); // <- Esto genera la UI en /swagger/index.html
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles(); // For serving uploaded images
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 
